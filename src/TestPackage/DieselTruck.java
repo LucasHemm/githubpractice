aa7b06a4a9a5ac0f71798c0ca75ae4822e0e976e
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-28 11:28:49 +0200

    Added the class DieselTruck.

---
+++ src/TestPackage/DieselTruck.java
@@ -0,0 +1,9 @@
+package TestPackage;
+
+public class DieselTruck extends DieselCar {
+
+
+    public DieselTruck(int kmPrLitre, boolean particleFilter, String registrationNumber, String make, String model, int numberOfDoors) {
+        super(kmPrLitre, particleFilter, registrationNumber, make, model, numberOfDoors);
+    }
+}
