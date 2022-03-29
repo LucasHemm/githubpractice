ba0dcfac7ae9e86c84fadbd8b9d1331bf5ea2e13
LucasHemm <99349701+LucasHemm@users.noreply.github.com>
2022-03-29 14:04:27 +0200

    added .gitignore

--- src/TestPackage/DieselTruck.java
+++ src/TestPackage/DieselTruck.java
@@ -2,6 +2,7 @@ package TestPackage;
 
 public class DieselTruck extends DieselCar {
 
+    private int number = 9;
 
     public DieselTruck(int kmPrLitre, boolean particleFilter, String registrationNumber, String make, String model, int numberOfDoors) {
         super(kmPrLitre, particleFilter, registrationNumber, make, model, numberOfDoors);
